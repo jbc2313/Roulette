f7e2f9c23f1dedf7df8431a58ab63f323e3f5c7b
jbc2313 <jamesbcorbin@gmail.com>
2022-04-28 22:31:18 -0500

    added Wheel element and Spin Button

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('helloworld');
+console.log('welcome to roulette, please enjoy!');
+
+let btnEl = document.getElementById('spinBtn');
+console.log(btnEl);
+
+let whlEl = document.getElementById('whl');
+console.log(whlEl);
+
+btnEl.addEventListener('click', () => {
+    console.log('you just clicked the spin button!');
+
+
+});
+
+
+
